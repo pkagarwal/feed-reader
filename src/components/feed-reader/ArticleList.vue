3be50db0734dd459e722cede6c82ea4c89fa9a63
P K Agarwal <pkagarwal@gmail.com>
2021-01-04 20:04:31 +0530

    Netlify chnaged

--- src/components/feed-reader/ArticleList.vue
+++ src/components/feed-reader/ArticleList.vue
@@ -25,7 +25,7 @@ export default {
     props: ['articles'],
     methods: {
         selectArticle(article) {
-            this.$emit(onSelectArticle, article)
+            this.$emit('onSelectArticle', article)
         }
     }
 }
