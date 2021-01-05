2bbedcddc20564404c91986e9f21ba610b7baa85
P K Agarwal <pkagarwal@gmail.com>
2021-01-05 11:38:58 +0530

    Chnage to feeds and add hello

--- src/lambda/feeds.js
+++ src/lambda/feeds.js
@@ -1,11 +1,11 @@
-const Parser = require("rss-parser")
+import Parser from 'rss-parser'
 
 
 export function handler(event, context, callback) {
   const FEEDS = [
-    'https://codepen.io/posts/feed',
-    'https://hnrss.org/frontpage',
-    'http://github-trends.ryotarai.info/rss/github_trends_javascript_daily.rss'
+    'https://codepen.io/posts/feed'
+    //'https://hnrss.org/frontpage',
+    //'http://github-trends.ryotarai.info/rss/github_trends_javascript_daily.rss'
   ]
 
   let parser = new Parser()
